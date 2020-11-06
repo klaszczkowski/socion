2fa1bdb0244f3b80ffd52c6857115126b42d083b
Karol Laszczkowski <studiograficzne01@gmail.com>
2020-11-06 17:10:13 +0100

    gant poc

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -11,6 +11,8 @@
           <ion-title size="large">Tab 3</ion-title>
         </ion-toolbar>
       </ion-header>
+
+      <svg id="gantt"></svg>
       
       <ExploreContainer name="Tab 3 page" />
     </ion-content>
@@ -23,6 +25,74 @@ import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default  {
   name: 'Tab3',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  mounted: function() {
+    const tasks = [
+  {
+    id: 'Task 1',
+    name: 'Redesign website 1',
+    start: '2016-12-28',
+    end: '2016-12-31',
+    progress: 20,
+    dependencies: 'Task 2',
+    custom_class: 'bar-milestone-1' // optional
+  },
+  {
+    id: 'Task 2',
+    name: 'Redesign website 2',
+    start: '2016-12-25',
+    end: '2017-01-02',
+    progress: 20,
+    dependencies: '',
+    custom_class: 'bar-milestone-1' // optional
+  },
+  {
+    id: 'Task 3',
+    name: 'Redesign website 3',
+    start: '2016-01-28',
+    end: '2016-02-20',
+    progress: 20,
+    dependencies: 'Task 1',
+    custom_class: 'bar-milestone-1' // optional
+  },
+  {
+    id: 'Task 4',
+    name: 'Redesign website 4',
+    start: '2016-12-28',
+    end: '2016-12-31',
+    progress: 20,
+    dependencies: 'Task 2, Task 3',
+    custom_class: 'bar-milestone', // optional
+    periods: [
+	{
+		start: '2017-01-05',
+	end: '2017-01-07'
+ },
+	{
+		start: '2017-01-09',
+		end: '2017-01-14'
+},
+	{
+		start: '2017-01-15',
+		end: '2017-01-17'
+  }
+   ]
+  }
+];
+const gantt = new Gantt("#gantt", tasks, {
+    header_height: 50,
+    column_width: 30,
+    step: 24,
+    view_modes: ['Quarter Day', 'Half Day', 'Day', 'Week', 'Month'],
+    bar_height: 20,
+    bar_corner_radius: 3,
+    arrow_curve: 5,
+    padding: 18,
+    view_mode: 'Day',   
+    date_format: 'YYYY-MM-DD',
+    custom_popup_html: null
+});
+  }
 }
 </script>
+
--- src/views/Tab4.vue
+++ src/views/Tab4.vue
@@ -12,7 +12,7 @@
         </ion-toolbar>
       </ion-header>
       
-      <ExploreContainer name="Tab 3 page" />
+      <ExploreContainer name="Tab 4 page" />
     </ion-content>
   </ion-page>
 </template>
@@ -22,7 +22,7 @@ import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue
 import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default  {
-  name: 'Tab4ś',
+  name: 'Tab4',
   components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
 }
 </script>
